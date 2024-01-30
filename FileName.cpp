260fceef774631e0fb9db2c1177b1ac09e53f65e
CGMW293 <138163744+CGMW293@users.noreply.github.com>
2024-01-30 08:20:23 -0800

    Update FileName.cpp

--- FileName.cpp
+++ FileName.cpp
@@ -8,18 +8,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         ZeroMemory(&si, sizeof(si));
         si.cb = sizeof(si);
         ZeroMemory(&pi, sizeof(pi));
-        LPWSTR calcPtr = new wchar_t[MAX_PATH];//why can i just use a string?
+        LPWSTR calcPtr = new wchar_t[MAX_PATH];//why cant i just use a string?
         wcscpy_s(calcPtr, MAX_PATH, L"C:\\Windows\\System32\\calc.exe");
-        CreateProcessW(NULL,   // No module name (use command line)
-            calcPtr,        // Command line
-            NULL,           // Process handle not inheritable
-            NULL,           // Thread handle not inheritable
-            FALSE,          // Set handle inheritance to FALSE
-            CREATE_NO_WINDOW,              // No creation flags
-            NULL,           // Use parent's environment block
-            NULL,           // Use parent's starting directory 
-            &si,            // Pointer to STARTUPINFO structure
-            &pi);         // Pointer to PROCESS_INFORMATION structure
+        CreateProcessW(NULL, calcPtr, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi);   
         delete[] calcPtr;
     }
     return 0;
